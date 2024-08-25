d861a05241df6547d1935d69562e03103a15708a
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2024-08-25 12:25:07 -0400

    Ruby Glass

--- src/main/java/net/artienia/netherifymod/block/ModBlocks.java
+++ src/main/java/net/artienia/netherifymod/block/ModBlocks.java
@@ -17,34 +17,51 @@ import net.minecraftforge.registries.RegistryObject;
 import java.util.function.Supplier;
 
 public class ModBlocks {
-    public static final DeferredRegister<Block> BLOCKS =
-            DeferredRegister.create(ForgeRegistries.BLOCKS, NetherifyMod.MOD_ID);
-
-    public static final RegistryObject<Block> RUBY_BLOCK = registerBlock("ruby_block",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).mapColor(MapColor.FIRE)));
-    public static final RegistryObject<Block> RAW_RUBY_BLOCK = registerBlock("raw_ruby_block",
-            () -> new RotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.RAW_IRON_BLOCK).mapColor(MapColor.FIRE).lightLevel((p_220871_) -> {
-                return 15;
-            })));
-
-    public static final RegistryObject<Block> BLEEDING_OBSIDIAN = registerBlock("bleeding_obsidian",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.CRYING_OBSIDIAN).mapColor(MapColor.FIRE)));
-    public static final RegistryObject<Block> RUBY_GLASS = registerBlock("ruby_glass",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.TINTED_GLASS).mapColor(MapColor.FIRE).explosionResistance(6F)));
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, NetherifyMod.MOD_ID);
 
 
-    public static final RegistryObject<Block> MOLTEN_RUBY_ORE = registerBlock("molten_ruby_ore",
-            () -> new MagmaXP(BlockBehaviour.Properties.copy(Blocks.MAGMA_BLOCK)
-                    .strength(2f).requiresCorrectToolForDrops(), UniformInt.of(4, 8)));
-    public static final RegistryObject<Block> NETHER_RUBY_ORE = registerBlock("nether_ruby_ore",
-            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.NETHERRACK)
-                    .strength(2f).requiresCorrectToolForDrops(), UniformInt.of(3, 6)));
-    public static final RegistryObject<Block> RUBINATED_BLACKSTONE = registerBlock("rubinated_blackstone",
-            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.BLACKSTONE)
-                    .strength(2f).requiresCorrectToolForDrops(), UniformInt.of(3, 6)));
+    public static final RegistryObject<Block> RUBY_BLOCK = registerBlock("ruby_block", () -> new Block(BlockBehaviour
+    .Properties.copy(Blocks.IRON_BLOCK)
+    .mapColor(MapColor.FIRE)
+    ));
+    public static final RegistryObject<Block> RAW_RUBY_BLOCK = registerBlock("raw_ruby_block", () -> new RotatedPillarBlock(BlockBehaviour
+    .Properties.copy(Blocks.RAW_IRON_BLOCK)
+    .mapColor(MapColor.FIRE)
+    .lightLevel((p_220871_) -> {return 15;})
+    ));
 
-    public static final RegistryObject<Block> FREEZER = registerBlock("freezer",
-            () -> new FreezerBlock(BlockBehaviour.Properties.copy(Blocks.COPPER_BLOCK).noOcclusion()));
+    public static final RegistryObject<Block> BLEEDING_OBSIDIAN = registerBlock("bleeding_obsidian", () -> new GlassBlock(BlockBehaviour
+    .Properties.copy(Blocks.CRYING_OBSIDIAN)
+    .mapColor(MapColor.FIRE)
+    ));
+    public static final RegistryObject<Block> RUBY_GLASS = registerBlock("ruby_glass", () -> new GlassBlock(BlockBehaviour
+    .Properties.copy(Blocks.GLASS)
+    .mapColor(MapColor.FIRE)
+    .explosionResistance(100F)
+    .noOcclusion()
+    ));
+
+    public static final RegistryObject<Block> MOLTEN_RUBY_ORE = registerBlock("molten_ruby_ore", () -> new MagmaXP(BlockBehaviour
+    .Properties.copy(Blocks.MAGMA_BLOCK)
+    .strength(2f)
+    .requiresCorrectToolForDrops(), UniformInt.of(4, 8)
+    ));
+    public static final RegistryObject<Block> NETHER_RUBY_ORE = registerBlock("nether_ruby_ore", () -> new DropExperienceBlock(BlockBehaviour
+    .Properties.copy(Blocks.NETHERRACK)
+    .strength(2f).requiresCorrectToolForDrops(), UniformInt.of(3, 6)
+    ));
+    public static final RegistryObject<Block> RUBINATED_BLACKSTONE = registerBlock("rubinated_blackstone", () -> new DropExperienceBlock(BlockBehaviour
+    .Properties.copy(Blocks.BLACKSTONE)
+    .strength(2f)
+    .requiresCorrectToolForDrops(), UniformInt.of(3, 6)
+    ));
+
+    public static final RegistryObject<Block> FREEZER = registerBlock("freezer", () -> new FreezerBlock(BlockBehaviour
+    .Properties.copy(Blocks.COPPER_BLOCK)
+    .noOcclusion()
+    ));
+
+
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn);
@@ -54,7 +71,6 @@ public class ModBlocks {
     private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
         return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
     }
-
 
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
