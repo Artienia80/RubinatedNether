885b7e78ce5749738f9233088cd9cf0329f2e579
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2024-08-30 23:53:17 -0400

    Falling Chandeliers

--- src/main/java/net/artienia/rubinated_nether/block/Chandelier.java
+++ src/main/java/net/artienia/rubinated_nether/block/Chandelier.java
@@ -2,10 +2,13 @@ package net.artienia.rubinated_nether.block;
 
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.item.FallingBlockEntity;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.BlockGetter;
@@ -25,5 +28,35 @@ public class Chandelier extends Block {
         return SHAPE;
     }
 
+    /**
+    * Update the provided state given the provided neighbor direction and neighbor state, returning a new state.
+    * For example, fences make their connections to the passed in state if possible, and wet concrete powder immediately
+    * returns its solidified counterpart.
+    * Note that this method should ideally consider only the specific direction passed in.
+    */
+    public BlockState updateShape(BlockState pState, Direction pFacing, BlockState pFacingState, LevelAccessor pLevel, BlockPos pCurrentPos, BlockPos pFacingPos) {
+        pLevel.scheduleTick(pCurrentPos, this, 2);
+        return super.updateShape(pState, pFacing, pFacingState, pLevel, pCurrentPos, pFacingPos);
+     }
+
+    public void tick(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {
+        if (!this.canSurvive(pState, pLevel, pPos)) {
+        spawnFallingChandelier(pState, pLevel, pPos);
+        }
+    }
+
+    public boolean canSurvive(BlockState pState, LevelReader pLevel, BlockPos pPos) {
+        return Block.canSupportCenter(pLevel, pPos.above(), Direction.UP);
+     }
+
+    private static void spawnFallingChandelier(BlockState pState, ServerLevel pLevel, BlockPos pPos) {
+        BlockPos.MutableBlockPos blockpos$mutableblockpos = pPos.mutable();
+  
+        BlockState blockstate = pLevel.getBlockState(blockpos$mutableblockpos);
+        FallingBlockEntity fallingblockentity = FallingBlockEntity.fall(pLevel, blockpos$mutableblockpos, blockstate);
+        int i = Math.max(1 + pPos.getY() - blockpos$mutableblockpos.getY(), 6);
+        float f = 1.0F * (float)i;
+        fallingblockentity.setHurtsEntities(f, 40);
+    }
 }
 
