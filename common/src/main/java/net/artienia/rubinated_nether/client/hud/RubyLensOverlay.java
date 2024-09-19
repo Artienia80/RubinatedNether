0b01f99a85249f18848779e89354f86a13521dee
sery <sery@sappho.company>
2024-09-19 14:15:52 +0200

    fix ruby lens rendering

--- common/src/main/java/net/artienia/rubinated_nether/client/hud/RubyLensOverlay.java
+++ common/src/main/java/net/artienia/rubinated_nether/client/hud/RubyLensOverlay.java
@@ -6,20 +6,21 @@ import net.artienia.rubinated_nether.RubinatedNether;
 import net.artienia.rubinated_nether.item.ModItems;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.player.Player;
 
 public class RubyLensOverlay {
 
     private static final ResourceLocation RUBY_OVERLAY = RubinatedNether.id("textures/misc/ruby_overlay.png");
 
     public static void renderHud(GuiGraphics graphics, float tickDelta) {
-        Player player = Minecraft.getInstance().player;
-        if(player != null && player.getItemBySlot(EquipmentSlot.HEAD).is(ModItems.RUBY_LENS.get())) {
+        Minecraft minecraft = Minecraft.getInstance();
+        LocalPlayer player = minecraft.player;
+        if(player != null && minecraft.options.getCameraType().isFirstPerson() && player.getItemBySlot(EquipmentSlot.HEAD).is(ModItems.RUBY_LENS.get())) {
             Window window = Minecraft.getInstance().getWindow();
             RenderSystem.enableBlend();
-            graphics.blit(RUBY_OVERLAY, 0, 0, 0, 0, window.getWidth(), window.getHeight(), 512, 256);
+            graphics.blit(RUBY_OVERLAY, 0, 0, -150, 0, 0, window.getWidth(), window.getHeight(), 512, 256);
         }
     }
 }
