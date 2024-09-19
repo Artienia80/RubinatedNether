4cdfe94bec817fca678cbbd8f72f2e5a363fb213
sery <sery@sappho.company>
2024-09-19 14:20:01 +0200

    right click equip

--- common/src/main/java/net/artienia/rubinated_nether/item/RubyLens.java
+++ common/src/main/java/net/artienia/rubinated_nether/item/RubyLens.java
@@ -1,7 +1,12 @@
 package net.artienia.rubinated_nether.item;
 
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 
 public class RubyLens extends Item implements Equipable {
     public RubyLens(Properties properties) {
@@ -9,7 +14,12 @@ public class RubyLens extends Item implements Equipable {
     }
 
     @Override
-    public EquipmentSlot getEquipmentSlot() {
+    public @NotNull InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+        return this.swapWithEquipmentSlot(this, level, player, usedHand);
+    }
+
+    @Override
+    public @NotNull EquipmentSlot getEquipmentSlot() {
         return EquipmentSlot.HEAD;
     }
 }
