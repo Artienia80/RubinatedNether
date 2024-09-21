a5d946078c63a6c119675db65f1d7dea6b968ba8
sery <sery@sappho.company>
2024-09-21 23:47:12 +0200

    update datagen

--- forge/src/main/java/net/artienia/rubinated_nether/forge/datagen/ModBlockStateProvider.java
+++ forge/src/main/java/net/artienia/rubinated_nether/forge/datagen/ModBlockStateProvider.java
@@ -7,6 +7,7 @@ import net.minecraft.data.PackOutput;
 
 
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.model.data.ModelData;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.client.model.generators.ModelFile;
@@ -14,6 +15,13 @@ import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlockStateProvider extends BlockStateProvider {
+
+    final ModelFile laserOn = new ModelFile.ExistingModelFile(RubinatedNether.id("block/ruby_laser_on"), this.models().existingFileHelper);
+    final ModelFile laserOff = new ModelFile.ExistingModelFile(RubinatedNether.id("block/ruby_laser"), this.models().existingFileHelper);
+    final ModelFile laserTintedOn = new ModelFile.ExistingModelFile(RubinatedNether.id("block/ruby_laser_tinted_on"), this.models().existingFileHelper);
+    final ModelFile laserTintedOff = new ModelFile.ExistingModelFile(RubinatedNether.id("block/ruby_laser_tinted"), this.models().existingFileHelper);
+
+
     public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
         super(output, RubinatedNether.MOD_ID, exFileHelper);
     }
@@ -26,12 +34,18 @@ public class ModBlockStateProvider extends BlockStateProvider {
         paneBlockWithRenderType(ModBlocks.RUBY_GLASS_PANE.get(), modLoc("block/ruby_glass"), modLoc("block/ruby_glass_pane_top"), "minecraft:translucent");
         paneBlockWithRenderType(ModBlocks.MOLTEN_RUBY_GLASS_PANE.get(), modLoc("block/molten_ruby_glass"), modLoc("block/molten_ruby_glass_pane_top"), "minecraft:translucent");
 
-        ModelFile laserOn = new ModelFile.ExistingModelFile(RubinatedNether.id("block/ruby_laser_on"), this.models().existingFileHelper);
-        ModelFile laserOff = new ModelFile.ExistingModelFile(RubinatedNether.id("block/ruby_laser"), this.models().existingFileHelper);
-        directionalBlock(ModBlocks.RUBY_LASER.get(), state -> state.getValue(RubyLaserBlock.POWER) > 0 ? laserOn : laserOff);
+        directionalBlock(ModBlocks.RUBY_LASER.get(), this::resolveRubyLaserModel);
         // blockWithItem(ModBlocks.MOLTEN_RUBY_ORE);
         // blockWithItem(ModBlocks.NETHER_RUBY_ORE);
         // blockWithItem(ModBlocks.RUBINATED_BLACKSTONE);
+    }
+
+    private ModelFile resolveRubyLaserModel(BlockState state) {
+        boolean tinted = state.getValue(RubyLaserBlock.TINTED),
+            on = state.getValue(RubyLaserBlock.POWER) > 0;
+
+        if(tinted) return on ? laserTintedOn : laserTintedOff;
+        else return on ? laserOn : laserOff;
     }
 
     private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
