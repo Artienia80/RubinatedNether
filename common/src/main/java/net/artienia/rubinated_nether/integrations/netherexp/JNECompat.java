005c828df300510a4ce55fe9c1a9375f53fa31ac
Artienia80 <87064705+Artienia80@users.noreply.github.com>
2024-11-11 21:31:02 +0100

    JNE recipe + advancements Spring Water, Soakstone recipes + adv

--- common/src/main/java/net/artienia/rubinated_nether/integrations/netherexp/JNECompat.java
+++ common/src/main/java/net/artienia/rubinated_nether/integrations/netherexp/JNECompat.java
@@ -4,6 +4,7 @@ import net.artienia.rubinated_nether.content.block.freezer.FreezerBlockEntity;
 import net.artienia.rubinated_nether.integrations.CompatHandler;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 public class JNECompat implements CompatHandler {
@@ -14,5 +15,8 @@ public class JNECompat implements CompatHandler {
     public void setup() {
         Block black_ice = BuiltInRegistries.BLOCK.get(new ResourceLocation("netherexp", "black_ice"));
         FreezerBlockEntity.addItemFreezingTime(black_ice, 3200);
+
+        Item ectoplasm_bucket = BuiltInRegistries.ITEM.get(new ResourceLocation("netherexp", "ectoplasm_bucket"));
+        FreezerBlockEntity.addItemFreezingTime(ectoplasm_bucket, 20000);
     }
 }
