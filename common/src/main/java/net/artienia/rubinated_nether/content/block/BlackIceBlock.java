20186bcc50f075c89ca157885f2698e168a6a200
Artienia80 <87064705+Artienia80@users.noreply.github.com>
2024-11-21 14:55:14 +0100

    just other stuff

--- common/src/main/java/net/artienia/rubinated_nether/content/block/BlackIceBlock.java
+++ common/src/main/java/net/artienia/rubinated_nether/content/block/BlackIceBlock.java
@@ -2,22 +2,14 @@ package net.artienia.rubinated_nether.content.block;
 
 import net.artienia.rubinated_nether.content.RNTags;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.FluidTags;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-import java.util.Iterator;
 
 public class BlackIceBlock extends Block {
 
@@ -32,12 +24,6 @@ public class BlackIceBlock extends Block {
     }
 
     @Override
-    public void onPlace(BlockState state, Level level, BlockPos pos, BlockState oldState, boolean isMoving) {
-        super.onPlace(state, level, pos, oldState, isMoving);
-        checkAndDrop(state, level, pos);
-    }
-
-    @Override
     public void neighborChanged(BlockState state, Level level, BlockPos pos, Block block, BlockPos fromPos, boolean isMoving) {
         super.neighborChanged(state, level, pos, block, fromPos, isMoving);
         checkAndDrop(state, level, pos);
@@ -46,10 +32,9 @@ public class BlackIceBlock extends Block {
     // Custom method to check support and drop the block if no valid block is below
     private void checkAndDrop(BlockState state, Level level, BlockPos pos) {
         if (!canSurvive(state, level, pos)) {
-            level.destroyBlock(pos, true); // Destroy the block and drop its item
+            level.destroyBlock(pos, false); // Destroy the block
         }
     }
-
 
     public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
         BlockState blockState = level.getBlockState(pos.below());
