745d7b1ed2413b5076da34a1efb62efc090129b5
Artienia80 <87064705+Artienia80@users.noreply.github.com>
2024-12-22 00:14:08 +0100

    pillar and chiseled block.

--- src/main/java/corundum/rubinated_nether/data/RNRecipeProvider.java
+++ src/main/java/corundum/rubinated_nether/data/RNRecipeProvider.java
@@ -5,6 +5,8 @@ import java.util.concurrent.CompletableFuture;
 import java.util.List;
 
 import corundum.rubinated_nether.content.RNBlocks;
+import corundum.rubinated_nether.content.RNItems;
+import corundum.rubinated_nether.content.RNTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.RecipeCategory;
@@ -12,6 +14,8 @@ import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Blocks;
+import net.neoforged.neoforge.common.Tags;
 
 public class RNRecipeProvider extends RecipeProvider {
 	protected RNRecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> provider) {
@@ -133,23 +137,39 @@ public class RNRecipeProvider extends RecipeProvider {
 				4
 		);
 
-		twoByTwo(
+		oneByTwo(
 				recipeOutput,
 				RNBlocks.ALTAR_STONE_BRICKS_SLAB,
 				RNBlocks.CHISELED_ALTAR_STONE_BRICKS,
 				1
 		);
 
-		twoByTwo(
+		oneByTwo(
 				recipeOutput,
 				RNBlocks.ALTAR_STONE,
 				RNBlocks.ALTAR_STONE_PILLAR,
 				2
 		);
 
+		ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, RNBlocks.RUBINATED_CHISELED_ALTAR_STONE_BRICKS, 8)
+				.define('X', RNBlocks.CHISELED_ALTAR_STONE_BRICKS)
+				.define('O', RNItems.RUBY_ITEM)
+				.pattern("XXX")
+				.pattern("XOX")
+				.pattern("XXX")
+				.unlockedBy(getHasName(RNBlocks.CHISELED_ALTAR_STONE_BRICKS), has(RNItems.RUBY_ITEM))
+				.save(recipeOutput);
+
+		ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, RNBlocks.BLEEDING_OBSIDIAN, 8)
+				.define('X', Blocks.CRYING_OBSIDIAN)
+				.define('O', RNItems.RUBY_ITEM)
+				.pattern("XXX")
+				.pattern("XOX")
+				.pattern("XXX")
+				.unlockedBy(getHasName(Blocks.CRYING_OBSIDIAN), has(RNItems.RUBY_ITEM))
+				.save(recipeOutput);
 	}
 
-	@SuppressWarnings("unused")
 	private void twoByTwo(RecipeOutput recipeOutput, ItemLike input, ItemLike output, int count) {
 		ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, output, count)
 				.define('I', input)
@@ -158,6 +178,7 @@ public class RNRecipeProvider extends RecipeProvider {
 				.unlockedBy(getHasName(input), has(input))
 				.save(recipeOutput);
 	}
+
 	private void oneByTwo(RecipeOutput recipeOutput, ItemLike input, ItemLike output, int count) {
 		ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, output, count)
 				.define('I', input)
@@ -166,6 +187,7 @@ public class RNRecipeProvider extends RecipeProvider {
 				.unlockedBy(getHasName(input), has(input))
 				.save(recipeOutput);
 	}
+
 	private void stairsAndSlab(RecipeOutput recipeOutput, ItemLike input, ItemLike stairs, ItemLike slab) {
 		ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, stairs, 4)
 				.define('S', input)
@@ -180,6 +202,7 @@ public class RNRecipeProvider extends RecipeProvider {
 				.unlockedBy(getHasName(input), has(input))
 				.save(recipeOutput);
 	}
+
 	private void wall(RecipeOutput recipeOutput, ItemLike input, ItemLike wall) {
 		ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, wall, 6)
 				.define('P', input)
