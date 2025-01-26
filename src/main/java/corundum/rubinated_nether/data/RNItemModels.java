091c9cd93ad5897aa2be26a632c64c285952ff68
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2025-01-26 12:50:25 -0500

    Better Item Datagen

--- src/main/java/corundum/rubinated_nether/data/RNItemModels.java
+++ src/main/java/corundum/rubinated_nether/data/RNItemModels.java
@@ -4,141 +4,157 @@ import corundum.rubinated_nether.RubinatedNether;
 import corundum.rubinated_nether.content.RNBlocks;
 import corundum.rubinated_nether.content.RNItems;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.registries.DeferredBlock;
 
 public class RNItemModels extends ItemModelProvider {
 	public RNItemModels(PackOutput output, ExistingFileHelper fileHelper) {
 		super(output, RubinatedNether.MODID, fileHelper);
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void registerModels() {
 		// Block items
-		this.simpleBlockItem(RNBlocks.NETHER_RUBY_ORE.get());
-		this.simpleBlockItem(RNBlocks.MOLTEN_RUBY_ORE.get());
-		this.simpleBlockItem(RNBlocks.RUBINATED_BLACKSTONE.get());
+		simpleBlockItems(
+			RNBlocks.NETHER_RUBY_ORE,
+			RNBlocks.MOLTEN_RUBY_ORE,
+			RNBlocks.RUBINATED_BLACKSTONE,
 
-		this.simpleBlockItem(RNBlocks.RUBY_BLOCK.get());
-		this.simpleBlockItem(RNBlocks.MOLTEN_RUBY_BLOCK.get());
+			RNBlocks.RUBY_BLOCK,
+			RNBlocks.MOLTEN_RUBY_BLOCK,
 
-		this.simpleBlockItem(RNBlocks.BLEEDING_OBSIDIAN.get());
+			RNBlocks.BLEEDING_OBSIDIAN,
 
-		this.basicItem(RNBlocks.RUBY_LANTERN.asItem());
-		this.basicItem(RNBlocks.CHANDELIER.asItem());
-		this.simpleBlockItem(RNBlocks.LAVA_LAMP.get());
-		this.simpleBlockItem(RNBlocks.DRY_ICE.get());
-		this.simpleBlockItem(RNBlocks.SOAKSTONE.get());
+			RNBlocks.LAVA_LAMP,
+			RNBlocks.DRY_ICE,
+			RNBlocks.SOAKSTONE,
 
-		this.simpleBlockItem(RNBlocks.BRAZIER.get());
+			RNBlocks.BRAZIER,
 
-		this.simpleBlockItem(RNBlocks.RUBY_GLASS.get());
+			RNBlocks.RUBY_GLASS,
+			RNBlocks.ORNATE_RUBY_GLASS,
+			RNBlocks.MOLTEN_RUBY_GLASS,
+
+			RNBlocks.SHRINE_STONE,
+			RNBlocks.POLISHED_SHRINE_STONE,
+			RNBlocks.POLISHED_SHRINE_STONE_STAIRS,
+			RNBlocks.POLISHED_SHRINE_STONE_SLAB,
+			RNBlocks.SHRINE_STONE_TILES,
+			RNBlocks.SHRINE_STONE_TILES_STAIRS,
+			RNBlocks.SHRINE_STONE_TILES_SLAB,
+			RNBlocks.SHRINE_STONE_PILLAR,
+			RNBlocks.SHRINE_STONE_BRICKS,
+			RNBlocks.SHRINE_STONE_BRICKS_STAIRS,
+			RNBlocks.SHRINE_STONE_BRICKS_SLAB,
+			RNBlocks.CHISELED_SHRINE_STONE_BRICKS,
+			RNBlocks.RUBINATED_CHISELED_SHRINE_STONE_BRICKS,
+			RNBlocks.RUBINATED_SHRINE_STONE_BRICKS,
+
+			RNBlocks.BRONZE_BLOCK,
+			RNBlocks.DISCOLORED_BRONZE_BLOCK,
+			RNBlocks.CORRODED_BRONZE_BLOCK,
+			RNBlocks.TARNISHED_BRONZE_BLOCK,
+			RNBlocks.CRYSTALLIZED_BRONZE_BLOCK,
+
+			RNBlocks.CUT_BRONZE_PILLAR,
+			RNBlocks.CUT_BRONZE_PILLAR_STAIRS,
+			RNBlocks.CUT_BRONZE_PILLAR_SLAB,
+			RNBlocks.DISCOLORED_CUT_BRONZE_PILLAR,
+			RNBlocks.DISCOLORED_CUT_BRONZE_PILLAR_STAIRS,
+			RNBlocks.DISCOLORED_CUT_BRONZE_PILLAR_SLAB,
+			RNBlocks.CORRODED_CUT_BRONZE_PILLAR,
+			RNBlocks.CORRODED_CUT_BRONZE_PILLAR_STAIRS,
+			RNBlocks.CORRODED_CUT_BRONZE_PILLAR_SLAB,
+			RNBlocks.TARNISHED_CUT_BRONZE_PILLAR,
+			RNBlocks.TARNISHED_CUT_BRONZE_PILLAR_STAIRS,
+			RNBlocks.TARNISHED_CUT_BRONZE_PILLAR_SLAB,
+			RNBlocks.CRYSTALLIZED_CUT_BRONZE_PILLAR,
+			RNBlocks.CRYSTALLIZED_CUT_BRONZE_PILLAR_STAIRS,
+			RNBlocks.CRYSTALLIZED_CUT_BRONZE_PILLAR_SLAB,
+
+			RNBlocks.CUT_BRONZE_BRICKS,
+			RNBlocks.CUT_BRONZE_BRICKS_STAIRS,
+			RNBlocks.CUT_BRONZE_BRICKS_SLAB,
+			RNBlocks.DISCOLORED_CUT_BRONZE_BRICKS,
+			RNBlocks.DISCOLORED_CUT_BRONZE_BRICKS_STAIRS,
+			RNBlocks.DISCOLORED_CUT_BRONZE_BRICKS_SLAB,
+			RNBlocks.CORRODED_CUT_BRONZE_BRICKS,
+			RNBlocks.CORRODED_CUT_BRONZE_BRICKS_STAIRS,
+			RNBlocks.CORRODED_CUT_BRONZE_BRICKS_SLAB,
+			RNBlocks.TARNISHED_CUT_BRONZE_BRICKS,
+			RNBlocks.TARNISHED_CUT_BRONZE_BRICKS_STAIRS,
+			RNBlocks.TARNISHED_CUT_BRONZE_BRICKS_SLAB,
+			RNBlocks.CRYSTALLIZED_CUT_BRONZE_BRICKS,
+			RNBlocks.CRYSTALLIZED_CUT_BRONZE_BRICKS_STAIRS,
+			RNBlocks.CRYSTALLIZED_CUT_BRONZE_BRICKS_SLAB
+		);
+
 		paneItem(
-			RNBlocks.RUBY_GLASS_PANE.getId().toString(),
+			RNBlocks.RUBY_GLASS_PANE,
 			"block/ruby_glass"
 		);
-		this.simpleBlockItem(RNBlocks.ORNATE_RUBY_GLASS.get());
 		paneItem(
-			RNBlocks.ORNATE_RUBY_GLASS_PANE.getId().toString(),
+			RNBlocks.ORNATE_RUBY_GLASS_PANE,
 			"block/ornate_ruby_glass"
 		);
-		this.simpleBlockItem(RNBlocks.MOLTEN_RUBY_GLASS.get());
 		paneItem(
-			RNBlocks.MOLTEN_RUBY_GLASS_PANE.getId().toString(),
+			RNBlocks.MOLTEN_RUBY_GLASS_PANE,
 			"block/molten_ruby_glass"
 		);
 
-		this.simpleBlockItem(RNBlocks.SHRINE_STONE.get());
-
-		this.simpleBlockItem(RNBlocks.POLISHED_SHRINE_STONE.get());
-		this.simpleBlockItem(RNBlocks.POLISHED_SHRINE_STONE_STAIRS.get());
-		this.simpleBlockItem(RNBlocks.POLISHED_SHRINE_STONE_SLAB.get());
 		wallInventory(
 				RNBlocks.POLISHED_SHRINE_STONE_WALL.getId().toString(),
 				modLoc("block/polished_shrine_stone")
 		);
-
-		this.simpleBlockItem(RNBlocks.SHRINE_STONE_TILES.get());
-		this.simpleBlockItem(RNBlocks.SHRINE_STONE_TILES_STAIRS.get());
-		this.simpleBlockItem(RNBlocks.SHRINE_STONE_TILES_SLAB.get());
 		wallInventory(
 				RNBlocks.SHRINE_STONE_TILES_WALL.getId().toString(),
 				modLoc("block/shrine_stone_tiles")
 		);
-
-		this.simpleBlockItem(RNBlocks.SHRINE_STONE_PILLAR.get());
-
-		this.simpleBlockItem(RNBlocks.SHRINE_STONE_BRICKS.get());
-		this.simpleBlockItem(RNBlocks.SHRINE_STONE_BRICKS_STAIRS.get());
-		this.simpleBlockItem(RNBlocks.SHRINE_STONE_BRICKS_SLAB.get());
 		wallInventory(
 				RNBlocks.SHRINE_STONE_BRICKS_WALL.getId().toString(),
 				modLoc("block/shrine_stone_bricks")
 		);
-		this.simpleBlockItem(RNBlocks.CHISELED_SHRINE_STONE_BRICKS.get());
-		this.simpleBlockItem(RNBlocks.RUBINATED_CHISELED_SHRINE_STONE_BRICKS.get());
-		this.simpleBlockItem(RNBlocks.RUBINATED_SHRINE_STONE_BRICKS.get());
-		this.basicItem(RNBlocks.RUNESTONE.asItem());
-		this.basicItem(RNItems.RUBY_LENS.asItem());
 
-		this.simpleBlockItem(RNBlocks.BRONZE_BLOCK.get());
-		this.simpleBlockItem(RNBlocks.DISCOLORED_BRONZE_BLOCK.get());
-		this.simpleBlockItem(RNBlocks.CORRODED_BRONZE_BLOCK.get());
-		this.simpleBlockItem(RNBlocks.TARNISHED_BRONZE_BLOCK.get());
-		this.simpleBlockItem(RNBlocks.CRYSTALLIZED_BRONZE_BLOCK.get());
+		basicItems(
+			// BlockItems with an Item Texture
+			RNBlocks.RUBY_LANTERN,
+			RNBlocks.CHANDELIER,
+	
+			RNBlocks.RUNESTONE,
+			RNItems.RUBY_LENS,
 
-		this.simpleBlockItem(RNBlocks.CUT_BRONZE_PILLAR.get());
-		this.simpleBlockItem(RNBlocks.CUT_BRONZE_PILLAR_STAIRS.get());
-		this.simpleBlockItem(RNBlocks.CUT_BRONZE_PILLAR_SLAB.get());
-		this.simpleBlockItem(RNBlocks.DISCOLORED_CUT_BRONZE_PILLAR.get());
-		this.simpleBlockItem(RNBlocks.DISCOLORED_CUT_BRONZE_PILLAR_STAIRS.get());
-		this.simpleBlockItem(RNBlocks.DISCOLORED_CUT_BRONZE_PILLAR_SLAB.get());
-		this.simpleBlockItem(RNBlocks.CORRODED_CUT_BRONZE_PILLAR.get());
-		this.simpleBlockItem(RNBlocks.CORRODED_CUT_BRONZE_PILLAR_STAIRS.get());
-		this.simpleBlockItem(RNBlocks.CORRODED_CUT_BRONZE_PILLAR_SLAB.get());
-		this.simpleBlockItem(RNBlocks.TARNISHED_CUT_BRONZE_PILLAR.get());
-		this.simpleBlockItem(RNBlocks.TARNISHED_CUT_BRONZE_PILLAR_STAIRS.get());
-		this.simpleBlockItem(RNBlocks.TARNISHED_CUT_BRONZE_PILLAR_SLAB.get());
-		this.simpleBlockItem(RNBlocks.CRYSTALLIZED_CUT_BRONZE_PILLAR.get());
-		this.simpleBlockItem(RNBlocks.CRYSTALLIZED_CUT_BRONZE_PILLAR_STAIRS.get());
-		this.simpleBlockItem(RNBlocks.CRYSTALLIZED_CUT_BRONZE_PILLAR_SLAB.get());
-
-		this.simpleBlockItem(RNBlocks.CUT_BRONZE_BRICKS.get());
-		this.simpleBlockItem(RNBlocks.CUT_BRONZE_BRICKS_STAIRS.get());
-		this.simpleBlockItem(RNBlocks.CUT_BRONZE_BRICKS_SLAB.get());
-		this.simpleBlockItem(RNBlocks.DISCOLORED_CUT_BRONZE_BRICKS.get());
-		this.simpleBlockItem(RNBlocks.DISCOLORED_CUT_BRONZE_BRICKS_STAIRS.get());
-		this.simpleBlockItem(RNBlocks.DISCOLORED_CUT_BRONZE_BRICKS_SLAB.get());
-		this.simpleBlockItem(RNBlocks.CORRODED_CUT_BRONZE_BRICKS.get());
-		this.simpleBlockItem(RNBlocks.CORRODED_CUT_BRONZE_BRICKS_STAIRS.get());
-		this.simpleBlockItem(RNBlocks.CORRODED_CUT_BRONZE_BRICKS_SLAB.get());
-		this.simpleBlockItem(RNBlocks.TARNISHED_CUT_BRONZE_BRICKS.get());
-		this.simpleBlockItem(RNBlocks.TARNISHED_CUT_BRONZE_BRICKS_STAIRS.get());
-		this.simpleBlockItem(RNBlocks.TARNISHED_CUT_BRONZE_BRICKS_SLAB.get());
-		this.simpleBlockItem(RNBlocks.CRYSTALLIZED_CUT_BRONZE_BRICKS.get());
-		this.simpleBlockItem(RNBlocks.CRYSTALLIZED_CUT_BRONZE_BRICKS_STAIRS.get());
-		this.simpleBlockItem(RNBlocks.CRYSTALLIZED_CUT_BRONZE_BRICKS_SLAB.get());
-
-
-		// Non-block items
-		this.basicItem(RNItems.RUBY_ITEM.get());
-		this.basicItem(RNItems.MOLTEN_RUBY_ITEM.get());
-		this.basicItem(RNItems.RUBY_SHARD_ITEM.get());
-		this.basicItem(RNItems.MOLTEN_RUBY_NUGGET_ITEM.get());
-		this.basicItem(RNItems.MUSIC_DISC_SHIMMER.get());
-		this.basicItem(RNItems.BRONZE_ROD.get());
-		this.basicItem(RNItems.BRONZE_SCRAP.get());
-		this.basicItem(RNItems.BRONZE_SHOT.get());
-
-
+			// Non-block items
+			RNItems.RUBY_ITEM,
+			RNItems.MOLTEN_RUBY_ITEM,
+			RNItems.RUBY_SHARD_ITEM,
+			RNItems.MOLTEN_RUBY_NUGGET_ITEM,
+			RNItems.MUSIC_DISC_SHIMMER,
+			RNItems.BRONZE_ROD,
+			RNItems.BRONZE_SCRAP,
+			RNItems.BRONZE_SHOT
+		);
 	}
 
-	private void paneItem(String block, String texture) {
-		this.withExistingParent(
-			block, 
+	private void paneItem(DeferredBlock<? extends Block> block, String texture) {
+		withExistingParent(
+			block.getId().toString(), 
 			mcLoc("item/generated")
 		)
 		.texture("layer0", texture)
 		.renderType(mcLoc("translucent"));
+	}
+
+	@SuppressWarnings("unchecked")
+	private void simpleBlockItems(DeferredBlock<? extends Block>... blocks) {
+		for (var block : blocks)
+			simpleBlockItem(block.get());
+	}
+	private void basicItems(ItemLike... items) {
+		for (var item : items)
+			basicItem(item.asItem());
 	}
 }
