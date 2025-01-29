3f03fc1331411e166a3dff02e3d4b0aded839ee3
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2025-01-28 22:12:03 -0500

    using {@link StringRepresentable.EnumCodec} is actually not necessary

--- src/main/java/corundum/rubinated_nether/content/recipe/RNBookCategory.java
+++ src/main/java/corundum/rubinated_nether/content/recipe/RNBookCategory.java
@@ -7,11 +7,7 @@ public enum RNBookCategory implements StringRepresentable {
 	FREEZABLE_MISC("freezable_misc"),
 	UNKNOWN("unknown");
 
-	/**
-	 * Warning for "deprecation" is suppressed because using {@link StringRepresentable.EnumCodec} is necessary.
-	 */
-	@SuppressWarnings("deprecation")
-	public static final StringRepresentable.EnumCodec<RNBookCategory> CODEC = StringRepresentable.fromEnum(RNBookCategory::values);
+	public static final StringRepresentableCodec<RNBookCategory> CODEC = StringRepresentable.fromEnum(RNBookCategory::values);
 	private final String name;
 
 	RNBookCategory(String name) {
