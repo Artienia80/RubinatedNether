6ce5d7433e44433cf0f1b63cce0b19f7509577be
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2025-01-29 00:06:19 -0500

    Some Code Cleanup
    I didnt fix anything but i made it look a bit nicer

--- src/main/java/corundum/rubinated_nether/client/render/RNRenderTypes.java
+++ src/main/java/corundum/rubinated_nether/client/render/RNRenderTypes.java
@@ -2,50 +2,65 @@ package corundum.rubinated_nether.client.render;
 
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat;
+
+import corundum.rubinated_nether.RubinatedNether;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
 
 public class RNRenderTypes extends RenderType {
-    public RNRenderTypes(String name, VertexFormat format, VertexFormat.Mode mode, int bufferSize, boolean affectsCrumbling, boolean sortOnUpload, Runnable setupState, Runnable clearState) {
-        super(name, format, mode, bufferSize, affectsCrumbling, sortOnUpload, setupState, clearState);
-    }
+	public RNRenderTypes(
+		String name, 
+		VertexFormat format, 
+		VertexFormat.Mode mode, 
+		int bufferSize, 
+		boolean affectsCrumbling, 
+		boolean sortOnUpload, 
+		Runnable setupState, 
+		Runnable clearState
+	) {
+		super(
+			name, 
+			format, 
+			mode, 
+			bufferSize, 
+			affectsCrumbling, 
+			sortOnUpload, 
+			setupState, 
+			clearState
+		);
+	}
 
-    public static final RenderType LASER_BEAM = RenderType.create(
-            "rubinated_nether:laser_beam",
-            DefaultVertexFormat.BLOCK,
-            VertexFormat.Mode.QUADS,
-            512,
-            false,
-            true,
-            CompositeState.builder()
-                    .setShaderState(new ShaderStateShard(GameRenderer::getRendertypeBeaconBeamShader))
-                    .setTextureState(new TextureStateShard(RubyLaserRenderer.LASER_TEXTURE, false, false))
-                    .setTransparencyState(RenderStateShard.TRANSLUCENT_TRANSPARENCY)
-                    .setWriteMaskState(RenderStateShard.COLOR_DEPTH_WRITE)
-                    .setCullState(RenderStateShard.CULL)
-                    .setLightmapState(RenderStateShard.NO_LIGHTMAP)
-                    .setOverlayState(RenderStateShard.NO_OVERLAY)
-                    .setOutputState(RenderStateShard.ITEM_ENTITY_TARGET)
-                    .createCompositeState(false)
-    );
+	public static final RenderType LASER_BEAM = createRenderType(
+		"laser_beam", 
+		RubyLaserRenderer.LASER_TEXTURE
+	);
 
-    public static final RenderType LASER_BEAM_GREYSCALE = RenderType.create(
-            "rubinated_nether:laser_beam_greyscale",
-            DefaultVertexFormat.BLOCK,
-            VertexFormat.Mode.QUADS,
-            512,
-            false,
-            true,
-            CompositeState.builder()
-                    .setShaderState(new ShaderStateShard(GameRenderer::getRendertypeBeaconBeamShader))
-                    .setTextureState(new TextureStateShard(RubyLaserRenderer.LASER_TEXTURE_GREYSCALE, false, false))
-                    .setTransparencyState(RenderStateShard.TRANSLUCENT_TRANSPARENCY)
-                    .setWriteMaskState(RenderStateShard.COLOR_DEPTH_WRITE)
-                    .setCullState(RenderStateShard.CULL)
-                    .setLightmapState(RenderStateShard.NO_LIGHTMAP)
-                    .setOverlayState(RenderStateShard.NO_OVERLAY)
-                    .setOutputState(RenderStateShard.ITEM_ENTITY_TARGET)
-                    .createCompositeState(false)
-    );
+	public static final RenderType LASER_BEAM_GRAYSCALE = createRenderType(
+		"laser_beam_grayscale", 
+		RubyLaserRenderer.LASER_TEXTURE_GREYSCALE
+	);
+
+	private static RenderType createRenderType(String name, ResourceLocation texture) {
+		return RenderType.create(
+			RubinatedNether.MODID + ":" + name,
+			DefaultVertexFormat.BLOCK,
+			VertexFormat.Mode.QUADS,
+			512,
+			false,
+			true,
+			CompositeState.builder()
+				.setShaderState(new ShaderStateShard(GameRenderer::getRendertypeBeaconBeamShader))
+				.setTextureState(new TextureStateShard(texture, false, false))
+				.setTransparencyState(RenderStateShard.TRANSLUCENT_TRANSPARENCY)
+
+				.setWriteMaskState(RenderStateShard.COLOR_DEPTH_WRITE)
+				.setCullState(RenderStateShard.CULL)
+				.setLightmapState(RenderStateShard.NO_LIGHTMAP)
+				.setOverlayState(RenderStateShard.NO_OVERLAY)
+				.setOutputState(RenderStateShard.MAIN_TARGET)
+				.createCompositeState(false)
+		);
+	}
 }
--- src/main/java/corundum/rubinated_nether/client/render/RubyLaserRenderer.java
+++ src/main/java/corundum/rubinated_nether/client/render/RubyLaserRenderer.java
@@ -1,14 +1,15 @@
 package corundum.rubinated_nether.client.render;
 
+import org.joml.Quaternionf;
+
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+
 import corundum.rubinated_nether.RubinatedNether;
 import corundum.rubinated_nether.content.RNItems;
 import corundum.rubinated_nether.content.blocks.RubyLaserBlock;
 import corundum.rubinated_nether.content.blocks.entities.RubyLaserBlockEntity;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
@@ -16,13 +17,11 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.Direction;
-import net.minecraft.core.Vec3i;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.FastColor;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.phys.AABB;
-import org.joml.Quaternionf;
 
 public class RubyLaserRenderer implements BlockEntityRenderer<RubyLaserBlockEntity> {
 	public static final ResourceLocation LASER_TEXTURE = RubinatedNether.id("textures/misc/ruby_laser_beam.png");
@@ -36,34 +35,41 @@ public class RubyLaserRenderer implements BlockEntityRenderer<RubyLaserBlockEnti
 	public RubyLaserRenderer(BlockEntityRendererProvider.Context context) {}
 
 	@Override
-	public void render(RubyLaserBlockEntity blockEntity, float partialTick, PoseStack poseStack, MultiBufferSource buffer, int packedLight, int packedOverlay) {
-		LocalPlayer player = Minecraft.getInstance().player;
-		ClientLevel level = Minecraft.getInstance().level;
+	public void render(
+		RubyLaserBlockEntity blockEntity, 
+		float partialTick, 
+		PoseStack poseStack, 
+		MultiBufferSource buffer, 
+		int packedLight, 
+		int packedOverlay
+	) {
+		var player = Minecraft.getInstance().player;
+		var level = Minecraft.getInstance().level;
 
-		if(blockEntity.alwaysVisible() || player.getItemBySlot(EquipmentSlot.HEAD).is(RNItems.RUBY_LENS.get())) {
+		if (blockEntity.alwaysVisible() || player.getItemBySlot(EquipmentSlot.HEAD).is(RNItems.RUBY_LENS.get())) {
 
 			poseStack.pushPose();
 			poseStack.translate(0.5f, 0.5f, 0.5f);
 
 			// laser facing
 			Direction facing = blockEntity.getBlockState().getValue(RubyLaserBlock.FACING);
-			float xRot = (facing == Direction.UP) ? 0 : (Direction.Plane.VERTICAL.test(facing) ? 180f : 90f);
-			float zRot = (Math.max(facing.get2DDataValue(), 0) & 3) * 90f;
+			var xRot = (facing == Direction.UP) ? 0 : (Direction.Plane.VERTICAL.test(facing) ? 180f : 90f);
+			var zRot = (Math.max(facing.get2DDataValue(), 0) & 3) * 90f;
 
 			tempQuat.rotationXYZ(xRot * Mth.DEG_TO_RAD, 0, zRot * Mth.DEG_TO_RAD);
 
 			// rotating animation
-			float lerpedTime = Mth.lerp(partialTick, level.getGameTime(), level.getGameTime() + 1);
-			float angle = (lerpedTime * 3) % 360f;
+			var lerpedTime = Mth.lerp(partialTick, level.getGameTime(), level.getGameTime() + 1);
+			var angle = (lerpedTime * 3) % 360f;
 			tempQuat.rotateY(angle * Mth.DEG_TO_RAD);
 
 			poseStack.mulPose(tempQuat);
 			poseStack.translate(-0.5f, -0.5f, -0.5f);
 
-			float maxY = (float) (blockEntity.getRenderRange() + 1f);
-			int color;
+			var maxY = (float) (blockEntity.getRenderRange() + 1f);
 
-			if(blockEntity.isColored()) {
+			int color;
+			if (blockEntity.isColored()) {
 				color = blockEntity.getColor().orElse(BASE_COLOR);
 			} else if(blockEntity.isSilly()) {
 				float hue = lerpedTime % 50f / 50f;
@@ -74,7 +80,7 @@ public class RubyLaserRenderer implements BlockEntityRenderer<RubyLaserBlockEnti
 			}
 
 			// Use fallback render type if shaders in use because beacon beam broken
-			VertexConsumer consumer = buffer.getBuffer(getRenderType(blockEntity.isColored() || blockEntity.isSilly()));
+			var consumer = buffer.getBuffer(getRenderType(blockEntity.isColored() || blockEntity.isSilly()));
 
 			renderFace(poseStack, consumer, .4f, 1, .6f, .6f, maxY, .6f, color, lerpedTime, Direction.NORTH);
 			renderFace(poseStack, consumer, .6f, 1, .4f, .4f, maxY, .4f, color, lerpedTime, Direction.SOUTH);
@@ -82,7 +88,6 @@ public class RubyLaserRenderer implements BlockEntityRenderer<RubyLaserBlockEnti
 			renderFace(poseStack, consumer, .6f, 1, .6f, .6f, maxY, .4f, color, lerpedTime, Direction.WEST);
 			poseStack.popPose();
 		}
-
 	}
 
 	private void renderFace(
@@ -99,14 +104,13 @@ public class RubyLaserRenderer implements BlockEntityRenderer<RubyLaserBlockEnti
 		Direction face
 	) {
 		PoseStack.Pose pose = matrices.last();
-		float maxV = (maxY - 1f) / 15f;
+		var maxV = (maxY - 1f) / 15f;
 
-		float v0 = 1 - (ticks % 150f) / 150f;
-		float v1 = v0 + (maxV * 0.4f);
+		var v0 = 1 - (ticks % 150f) / 150f;
+		var v1 = v0 + (maxV * 0.4f);
 
-		int endAlpha = (int)(maxY) - 16;
 		var endColor = FastColor.ARGB32.color(
-			-endAlpha * 15,
+			-((int)(maxY) - 16) * 15,
 			color
 		);
 
@@ -140,16 +144,15 @@ public class RubyLaserRenderer implements BlockEntityRenderer<RubyLaserBlockEnti
 	}
 
 	public AABB getRenderBoundingBox(RubyLaserBlockEntity blockEntity) {
-		Direction facing = blockEntity.getBlockState().getValue(RubyLaserBlock.FACING);
-		Vec3i end = facing.getNormal().multiply(blockEntity.getCurrentRange() + 1);
+		var facing = blockEntity.getBlockState().getValue(RubyLaserBlock.FACING);
+		var end = facing.getNormal().multiply(blockEntity.getCurrentRange() + 1);
 		return new AABB(blockEntity.getBlockPos()).expandTowards(end.getX(), end.getY(), end.getZ());
 	}
 
 	protected RenderType getRenderType(boolean colored) {
-		if(ShaderHelper.isShaderPackInUse()) {
+		if (ShaderHelper.isShaderPackInUse())
 			return RenderType.entityTranslucentEmissive(colored ? LASER_TEXTURE_GREYSCALE : LASER_TEXTURE);
-		} else {
-			return colored ? RNRenderTypes.LASER_BEAM_GREYSCALE : RNRenderTypes.LASER_BEAM;
-		}
+		else
+			return colored ? RNRenderTypes.LASER_BEAM_GRAYSCALE : RNRenderTypes.LASER_BEAM;
 	}
 }
