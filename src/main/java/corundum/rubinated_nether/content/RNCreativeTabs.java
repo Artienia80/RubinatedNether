e04a45218bc3fe237263f957afa45d44d6bb43c9
Artienia80 <87064705+Artienia80@users.noreply.github.com>
2025-04-13 01:46:37 +0200

    creative tab

--- src/main/java/corundum/rubinated_nether/content/RNCreativeTabs.java
+++ src/main/java/corundum/rubinated_nether/content/RNCreativeTabs.java
@@ -134,6 +134,29 @@ public class RNCreativeTabs {
 					RNBlocks.CRYSTALLIZED_CUT_BRONZE_BRICKS_SLAB,
 					RNBlocks.CRYSTALLIZED_BRONZE_BULB
 				);
+
+				addItems(
+						output,
+
+						RNItems.GREED_RUNE,
+						RNItems.WRATH_RUNE,
+						RNItems.SLOTH_RUNE,
+						RNItems.GLUTTONY_RUNE,
+						RNItems.ENVY_RUNE,
+						RNItems.VAINGLORY_RUNE,
+						RNItems.PRIDE_RUNE,
+						RNItems.ACEDIA_RUNE,
+						RNItems.LUXURIA_RUNE,
+						RNItems.INSIDIAE_RUNE,
+						RNItems.SUPERBIA_RUNE,
+						RNItems.TRISTIA_RUNE,
+						RNItems.STUDIOSE_RUNE,
+						RNItems.ARDENTER_RUNE,
+						RNItems.NIMIS_RUNE
+
+				);
+
+
 			})
 			.build()
 	);
