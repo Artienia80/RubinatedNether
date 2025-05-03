1675f2667340c0767ef9d1746c9748a6b594fed6
Artienia80 <87064705+Artienia80@users.noreply.github.com>
2025-05-03 17:12:12 +0200

    working loot

--- src/main/java/corundum/rubinated_nether/content/RNLootInjector.java
+++ src/main/java/corundum/rubinated_nether/content/RNLootInjector.java
@@ -24,34 +24,38 @@ public class RNLootInjector {
 
         // Add runes to loot tables with 2.5% chance
         switch (name.toString()) {
-            case "minecraft:chests/end_city":
-                addRune(event, GREED_RUNE.get(), 0.025f);
-                addRune(event, ENVY_RUNE.get(), 0.025f);
+            case "minecraft:chests/end_city_treasure":
+                addRune(event, GREED_RUNE.get(), 0.05f);
+                addRune(event, ENVY_RUNE.get(), 0.05f);
                 break;
             case "minecraft:chests/ancient_city":
-                addRune(event, GLUTTONY_RUNE.get(), 0.025f);
-                addRune(event, WRATH_RUNE.get(), 0.025f);
+                addRune(event, GLUTTONY_RUNE.get(), 0.05f);
+                addRune(event, WRATH_RUNE.get(), 0.05f);
                 break;
-            case "minecraft:chests/jungle_temple":
-                addRune(event, LUXURIA_RUNE.get(), 0.025f);
-                addRune(event, PRIDE_RUNE.get(), 0.025f);
+            case "minecraft:chests/buried_treasure":
+                addRune(event, LUXURIA_RUNE.get(), 0.05f);
+                addRune(event, PRIDE_RUNE.get(), 0.05f);
                 break;
             case "minecraft:chests/stronghold_library":
-                addRune(event, SUPERBIA_RUNE.get(), 0.025f);
-                addRune(event, ARDENTER_RUNE.get(), 0.025f);
+                addRune(event, SUPERBIA_RUNE.get(), 0.05f);
+                addRune(event, ARDENTER_RUNE.get(), 0.05f);
                 break;
             case "minecraft:chests/nether_bridge":
-            case "minecraft:chests/bastion_treasure":
-            case "minecraft:chests/underwater_ruin_big":
-                addRune(event, SLOTH_RUNE.get(), 0.025f);
-                addRune(event, VAINGLORY_RUNE.get(), 0.025f);
-                addRune(event, INSIDIAE_RUNE.get(), 0.025f);
-                addRune(event, STUDIOSE_RUNE.get(), 0.025f);
+                addRune(event, SLOTH_RUNE.get(), 0.05f);
                 break;
-            case "minecraft:chests/trial_chambers_reward":
-                addRune(event, NIMIS_RUNE.get(), 0.025f);
-                addRune(event, TRISTIA_RUNE.get(), 0.025f);
-                addRune(event, ACEDIA_RUNE.get(), 0.025f);
+            case "minecraft:chests/bastion_treasure":
+                addRune(event, VAINGLORY_RUNE.get(), 0.05f);
+                break;
+            case "minecraft:chests/underwater_ruin_big":
+                addRune(event, INSIDIAE_RUNE.get(), 0.05f);
+                break;
+            case "minecraft:chests/woodland_mansion":
+                addRune(event, STUDIOSE_RUNE.get(), 0.05f);
+                break;
+            case "minecraft:chests/trial_chambers/reward_unique":
+                addRune(event, NIMIS_RUNE.get(), 0.1f);
+                addRune(event, TRISTIA_RUNE.get(), 0.1f);
+                addRune(event, ACEDIA_RUNE.get(), 0.1f);
                 break;
         }
     }
