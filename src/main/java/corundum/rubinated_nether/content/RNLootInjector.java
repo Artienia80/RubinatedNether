d57fa9635acab8f2d02d706d9f5d6f24d0245c38
Artienia80 <87064705+Artienia80@users.noreply.github.com>
2025-05-03 15:19:13 +0200

    loottable pre-build test

---
+++ src/main/java/corundum/rubinated_nether/content/RNLootInjector.java
@@ -0,0 +1,64 @@
+package corundum.rubinated_nether.content;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.LootTableLoadEvent;
+
+import static corundum.rubinated_nether.content.RNItems.*;
+
+public class ModLootInjector {
+
+    public static void init() {
+        NeoForge.EVENT_BUS.register(ModLootInjector.class);
+    }
+
+    @SubscribeEvent
+    public static void onLootTableLoad(LootTableLoadEvent event) {
+        ResourceLocation name = event.getName();
+
+        // Add runes to loot tables with 2.5% chance
+        if (name.equals(ResourceLocation.fromNamespaceAndPath("minecraft", "chests/end_city"))) {
+            addRune(event, GREED_RUNE.get(), 0.025f);
+            addRune(event, ENVY_RUNE.get(), 0.025f);
+        } else if (name.equals(ResourceLocation.fromNamespaceAndPath("minecraft", "chests/ancient_city"))) {
+            addRune(event, GLUTTONY_RUNE.get(), 0.025f);
+            addRune(event, WRATH_RUNE.get(), 0.025f);
+        } else if (name.equals(ResourceLocation.fromNamespaceAndPath("minecraft", "chests/jungle_temple"))) {
+            addRune(event, LUXURIA_RUNE.get(), 0.025f);
+            addRune(event, PRIDE_RUNE.get(), 0.025f);
+        } else if (name.equals(ResourceLocation.fromNamespaceAndPath("minecraft", "chests/stronghold_library"))) {
+            addRune(event, SUPERBIA_RUNE.get(), 0.025f);
+            addRune(event, ARDENTER_RUNE.get(), 0.025f);
+        } else if (
+                name.equals(ResourceLocation.fromNamespaceAndPath("minecraft", "chests/nether_bridge")) ||
+                        name.equals(ResourceLocation.fromNamespaceAndPath("minecraft", "chests/bastion_treasure")) ||
+                        name.equals(ResourceLocation.fromNamespaceAndPath("minecraft", "chests/underwater_ruin_big"))
+        ) {
+            addRune(event, SLOTH_RUNE.get(), 0.025f);
+            addRune(event, VAINGLORY_RUNE.get(), 0.025f);
+            addRune(event, INSIDIAE_RUNE.get(), 0.025f);
+            addRune(event, STUDIOSE_RUNE.get(), 0.025f);
+        } else if (name.equals(ResourceLocation.fromNamespaceAndPath("minecraft", "chests/trial_chambers_reward"))) {
+            addRune(event, NIMIS_RUNE.get(), 0.025f);
+            addRune(event, TRISTIA_RUNE.get(), 0.025f);
+            addRune(event, ACEDIA_RUNE.get(), 0.025f);
+        }
+    }
+
+    private static void addRune(LootTableLoadEvent event, Item item, float chance) {
+        LootPool pool = LootPool.lootPool()
+                .add(LootItem.lootTableItem(item)
+                        .when(LootItemRandomChanceCondition.randomChance(chance)))
+                .setRolls(ConstantValue.exactly(1))
+                .build();
+
+        // Add the pool to the loot table
+        event.getTable().addPool(pool);
+    }
+}
