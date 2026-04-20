361d90a2c9df08f385dfa012028aebff8e7584ac
darksonic300 <fabiross096@gmail.com>
2026-04-20 23:17:50 +0200

    feat: cranking effects fix

--- src/main/java/corundum/rubinated_nether/content/blocks/GearboxBlock.java
+++ src/main/java/corundum/rubinated_nether/content/blocks/GearboxBlock.java
@@ -111,64 +111,19 @@ public class GearboxBlock extends TarnishingBronzeBlock implements BEBlock<Gearb
 
     @Override
     protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
-        int crankCount = state.getValue(CRANK_LEVEL);
-        if (crankCount > 0 && crankCount < MAX_CRANK) {
-            decreaseCrankLevel(level, state, pos);
-        } else if (crankCount == MAX_CRANK) {
-            state = state.setValue(CRANK_LEVEL, 0);
-            state = state.setValue(LIT, true);
-            level.setBlock(pos, state, 3);
+        if(!level.isClientSide()) {
+            int crankCount = state.getValue(CRANK_LEVEL);
+            if (crankCount > 0 && crankCount < MAX_CRANK) {
+                decreaseCrankLevel(level, state, pos);
+            } else if (crankCount == MAX_CRANK) {
+                state = state.setValue(CRANK_LEVEL, 0);
+                state = state.setValue(LIT, true);
+                level.playSound(null, pos, SoundEvents.FIRE_EXTINGUISH, SoundSource.BLOCKS);
+                level.setBlock(pos, state, 3);
+            }
         }
         super.tick(state, level, pos, random);
     }
-
-    @Override
-    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
-        if(!level.isClientSide() && this.isCrankable(state)) {
-            increaseCrankLevel(level, state, pos);
-            return InteractionResult.sidedSuccess(level.isClientSide());
-        }
-        return super.useWithoutItem(state, level, pos, player, hitResult);
-    }
-
-    private boolean isCrankable(BlockState state) {
-        return !state.getValue(LIT);
-    }
-
-    private static void increaseCrankLevel(Level level, BlockState state, BlockPos pos) {
-        state = state.setValue(CRANK_LEVEL, Mth.clamp(state.getValue(CRANK_LEVEL) + 1, 0, MAX_CRANK));
-        level.setBlock(pos, state, 3);
-        level.scheduleTick(pos, state.getBlock(), 20);
-        System.out.println("Current State: " + state.getValue(CRANK_LEVEL));
-    }
-
-    private static void decreaseCrankLevel(Level level, BlockState state, BlockPos pos) {
-        state = state.setValue(CRANK_LEVEL, Mth.clamp(state.getValue(CRANK_LEVEL) - 1, 0, MAX_CRANK));
-        level.setBlock(pos, state, 3);
-        level.scheduleTick(pos, state.getBlock(), 20);
-        System.out.println("Current State: " + state.getValue(CRANK_LEVEL));
-    }
-
-
-    protected void neighborChanged(BlockState state, Level level, BlockPos pos, Block neighborBlock, BlockPos neighborPos, boolean movedByPiston) {
-        if (level instanceof ServerLevel serverlevel) {
-            this.checkAndFlip(state, serverlevel, pos);
-        }
-    }
-
-    public void checkAndFlip(BlockState state, ServerLevel level, BlockPos pos) {
-        boolean flag = level.hasNeighborSignal(pos);
-        if (flag != state.getValue(POWERED)) {
-            BlockState blockstate = state;
-            if (!(Boolean)state.getValue(POWERED)) {
-                blockstate = state.cycle(LIT);
-                //TODO: Same sound, but different registration to be made
-                level.playSound(null, pos, SoundEvents.FIRE_EXTINGUISH, SoundSource.BLOCKS);
-            }
-            level.setBlock(pos, blockstate.setValue(POWERED, flag), 3);
-        }
-    }
-
 
     @Override
     public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource random) {
@@ -199,12 +154,58 @@ public class GearboxBlock extends TarnishingBronzeBlock implements BEBlock<Gearb
         super.animateTick(state, level, pos, random);
     }
 
+    @Override
+    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
+        if(!level.isClientSide() && this.isCrankable(state)) {
+            increaseCrankLevel(level, state, pos);
+        }
+        return InteractionResult.sidedSuccess(level.isClientSide());
+    }
+
+    private boolean isCrankable(BlockState state) {
+        return !state.getValue(LIT);
+    }
+
+    private static void increaseCrankLevel(Level level, BlockState state, BlockPos pos) {
+        state = state.setValue(CRANK_LEVEL, Mth.clamp(state.getValue(CRANK_LEVEL) + 1, 0, MAX_CRANK));
+        level.setBlock(pos, state, 3);
+        level.playSound(null, pos, SoundEvents.LEVER_CLICK, SoundSource.BLOCKS, 1.0F, 1.0F);
+        level.scheduleTick(pos, state.getBlock(), 20);
+    }
+
+    private static void decreaseCrankLevel(Level level, BlockState state, BlockPos pos) {
+        state = state.setValue(CRANK_LEVEL, Mth.clamp(state.getValue(CRANK_LEVEL) - 1, 0, MAX_CRANK));
+        level.setBlock(pos, state, 3);
+        level.playSound(null, pos, SoundEvents.LEVER_CLICK, SoundSource.BLOCKS, 1.0F, 1.0F);
+        level.scheduleTick(pos, state.getBlock(), 20);
+    }
+
+
+    protected void neighborChanged(BlockState state, Level level, BlockPos pos, Block neighborBlock, BlockPos neighborPos, boolean movedByPiston) {
+        if (level instanceof ServerLevel serverlevel) {
+            this.checkAndFlip(state, serverlevel, pos);
+        }
+    }
+
+    public void checkAndFlip(BlockState state, ServerLevel level, BlockPos pos) {
+        boolean flag = level.hasNeighborSignal(pos);
+        if (flag != state.getValue(POWERED)) {
+            BlockState blockstate = state;
+            if (!(Boolean)state.getValue(POWERED)) {
+                blockstate = state.cycle(LIT);
+                //TODO: Same sound, but different registration to be made
+                level.playSound(null, pos, SoundEvents.FIRE_EXTINGUISH, SoundSource.BLOCKS);
+            }
+            level.setBlock(pos, blockstate.setValue(POWERED, flag), 3);
+        }
+    }
+
     public void appendHoverText(ItemStack itemStack, Item.TooltipContext tooltipContext, List<Component> components, TooltipFlag tooltipFlag) {
         super.appendHoverText(itemStack, tooltipContext, components, tooltipFlag);
         Spawner.appendHoverText(itemStack, components, "spawn_data");
     }
 
-    public static boolean noViewBlocking(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos) {
+    public static boolean noViewBlocking(BlockState _1, BlockGetter _2, BlockPos _3) {
         return false;
     }
 }
